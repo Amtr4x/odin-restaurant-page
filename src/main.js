bb7a8ded7b6224b4e387d8007ac2c85d3eb42908
leandryscsec <leandryscsec@gmail.com>
2024-08-01 17:58:23 -0400

    ADD main menu functionality

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
+const menuBar = document.querySelector("nav");
 
+menuBar.addEventListener("click", (btn) => {
+  const pressedBtn = btn.target.id;
+
+  switch (pressedBtn) {
+    case "home-section":
+      console.log("home");
+      break;
+    case "menu-section":
+      console.log("menu");
+      break;
+    case "about-section":
+      console.log("about");
+      break;
+  }
+});
